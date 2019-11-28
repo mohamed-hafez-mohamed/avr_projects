2a752c74dbf021c72318c138007dd30f968a45dd
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2019-11-28 14:33:12 +0200

    Update UART.c

--- Door_Locker/MC2/UART.c
+++ Door_Locker/MC2/UART.c
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * UART.c
  *
  * Created: 9/16/2019 04:17:24 م
@@ -9,7 +9,7 @@
 
 void  uart_baudrate(uartbaudrate_cnfg baudrate)
 {
-	ACCESS_REG_8BIT((UARTBASE + UBRRL_OFFSET)) = baudrate;
+	ACCESS_REG_8BIT((UARTBASE + UBRRL_OFFSET)) = baudrate;            // baud rate setting
 }
 
 void  uart_setframe(uartdata_bits databits,uartparity_cnfg parity,uartstopbit_cnfg stopbits)
@@ -17,6 +17,7 @@ void  uart_setframe(uartdata_bits databits,uartparity_cnfg parity,uartstopbit_cn
 	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (SET << URSEL);
 	switch(databits)
 	{
+		//configuration for different transmitted and received bits(5,6,7,8,9)
 		case _5BIT :
 		   CLEAR_BIT((UARTBASE + UCSRC_OFFSET),UCSZ0);
 		   CLEAR_BIT((UARTBASE + UCSRC_OFFSET),UCSZ1);
@@ -44,28 +45,28 @@ void  uart_setframe(uartdata_bits databits,uartparity_cnfg parity,uartstopbit_cn
 		break;
 	}
 	
-	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (parity   << UPM0);
-	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (stopbits << USBS);
+	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (parity   << UPM0);//no parity 
+	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (stopbits << USBS);//1stop , 
 }
 
 void  uart_init(uartstate_cnfg state,uartoperation_cnfg operation,uartclock_cnfg colck)
 {
-	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (operation << UMSEL);
+	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (operation << UMSEL); //configuration operation (sync or async)
 	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (colck     << UCPOL);
 	switch(state)
 	{
 		case RECEIVE :
-		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << RXEN);
+		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << RXEN); // wake up uart for recieve 
 		break;
 		case TRANSMIT:
-		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << TXEN);
+		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << TXEN); // wake up uart transmit
 		break;
 	}
 }
 
 void  uart_transmit(uint8 data,uint8 databits)
 {
-	while (!(ACCESS_REG_8BIT((UARTBASE + UCSRA_OFFSET)) & (1 << UDRE)));
+	while (!(ACCESS_REG_8BIT((UARTBASE + UCSRA_OFFSET)) & (1 << UDRE))); // checking if there's still data in Buffer
 	switch(databits)
 	{
 		case _5BIT :
@@ -82,14 +83,14 @@ void  uart_transmit(uint8 data,uint8 databits)
 
 uint8 uart_receive(uint8 databits)
 {
-	while (!(ACCESS_REG_8BIT((UARTBASE + UCSRA_OFFSET)) & (1 << RXC)));
+	while (!(ACCESS_REG_8BIT((UARTBASE + UCSRA_OFFSET)) & (1 << RXC))); // checking is that recieve completed
 	switch(databits)
 	{
 		case _5BIT :
 		case _6BIT :
 		case _7BIT :
 		case _8BIT :
-		  return READ_REG_8BIT((UARTBASE + USART_OFFSET));
+		  return READ_REG_8BIT((UARTBASE + USART_OFFSET)); 
 		break;
 		case _9BIT :
 		
