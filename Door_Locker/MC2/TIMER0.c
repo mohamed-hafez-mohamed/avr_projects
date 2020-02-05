51369698da3bc29e3656ee6b6e74896a3a897162
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2020-02-05 08:55:13 +0200

    Update TIMER0.c

--- Door_Locker/MC2/TIMER0.c
+++ Door_Locker/MC2/TIMER0.c
@@ -1,18 +1,25 @@
-﻿/*
+/*
  * TIMER0.c
  *
- * Created: 9/10/2019 02:20:45 م
+ * Created: 9/10/2019 02:20:45 ?
  *  Author: mah_h
  */ 
 
 #include "GPIO.h"
 #include "TIMER0.h"
 
+static void (*pfunc_ovi)(void);
+static void (*pfunc_oci)(void);
+
 void timer0_init(timer0_cnfg* ptrtimer0)
 {
-	ACCESS_REG_8BIT((TIMER0BASE + TIMER0_OFFSET))   = CLEAR_REGISTER;                 //clear TCNT0 REG
+	pin_dirc(T0,PIN_INPUT);
+	ACCESS_REG_8BIT((TIMER0BASE + TIMER0_OFFSET))    = CLEAR_REGISTER;                 //clear TCNT0 REG
 	ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))  = CLEAR_REGISTER;                 //clear TCCR0 REG
 	ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET)) |= (ptrtimer0->Timer0_clkSelect << CS00);
+	ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET)) |= (ptrtimer0->Timer0_COM       << COM00);
+	ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET))     |=  (ptrtimer0->Timer0_TOIE     << TOIE0);
+	ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET))     |=  (ptrtimer0->Timer0_OCIE     << OCIE0);
 	switch(ptrtimer0->Timer0_mode)
 	{
 		case Normal :
@@ -21,33 +28,66 @@ void timer0_init(timer0_cnfg* ptrtimer0)
 		break;
 		case PWM_phase_correct :
 		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   &= ~(SET << WGM01);
-		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |= (SET << WGM00);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM00);
+		   pin_dirc(OC0,PIN_OUTPUT);
 		break;
 		case CTC :
-		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |= (SET << WGM01);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM01);
 		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   &= ~(SET << WGM00);
+		   pin_dirc(OC0,PIN_OUTPUT);
 		break;
 		case Fast_PWM :
-		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |= (SET << WGM01);
-		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |= (SET << WGM00);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM01);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM00);
+		   pin_dirc(OC0,PIN_OUTPUT);
 		break;
 	}
-	switch(ptrtimer0->Timer0_TOIE)
+}
+
+uint8 ocr0(uint8 mode, uint8 value)
+{
+	switch(mode)
 	{
-		case TOIE0_enable :
-		   ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET)) |=  (SET << TOIE0);
+		case 'w' :
+		   WRITE_REG_8BIT((TIMER0BASE + OUTPUTCOM0_OFFSET), value);
 		break;
-		case TOIE0_disable :
-		   ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET)) &= ~(SET << TOIE0);
+		case 'r' :
+		   return ACCESS_REG_8BIT(TIMER0BASE + OUTPUTCOM0_OFFSET);
 		break;
 	}
-	switch(ptrtimer0->Timer0_OCIE)
+}
+
+uint8 tcnt0(uint8 mode, uint8 value)
+{
+	switch(mode)
 	{
-		case OCIE0_enable :
-		   ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET)) |=  (SET << OCIE0);
+		case 'w' :
+		   WRITE_REG_8BIT((TIMER0BASE + OUTPUTCOM0_OFFSET), value);
 		break;
-		case OCIE0_disable :
-		   ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET)) &= ~(SET << OCIE0);
+		case 'r' :
+		   return ACCESS_REG_8BIT(TIMER0BASE + TIMER0_OFFSET);
 		break;
 	}
+}
+
+void ov_int(void (*ptr_ISR)(void))
+{
+	pfunc_ovi = ptr_ISR;
+}
+
+void oc_int(void (*ptr_ISR)(void))
+{
+	pfunc_oci = ptr_ISR;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+	sei();
+	(*pfunc_ovi)();
+}
+
+ISR(TIMER0_COMP_vect)
+{
+	sei();
+	(*pfunc_oci)();
 }
